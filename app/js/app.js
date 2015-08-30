1872c0293bd5cd00bff57c2f4ed9ddec22f00584
Connie <cleung334@gmail.com>
2015-08-30 20:17:31 +0800

    Add page transition
    Signed-off-by: Connie <cleung334@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -5,15 +5,14 @@
 angular.module('myApp', [
   'ngRoute',
   'ngTouch',
+  'ngAnimate',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/add-expense', {templateUrl: 'partials/add-expense.html', controller: 'AddExpenseCtrl'});
   $routeProvider.when('/view-summary', {templateUrl: 'partials/view-summary.html', controller: 'ViewSummaryCtrl'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view-summary'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -33,13 +33,14 @@ angular.module('myApp.controllers', [])
   .controller('NavigationCtrl', ['$scope', '$location', function($scope, $location) {
 
     var navigator = function _navigator(incrementer) {
-      var pages = ['/', '/add-expense', '/view-summary'];
+      var pages = ['/add-expense', '/view-summary'];
 
       var nextUrl = "";
       var currentPage = $location.path();
       var lastPageIndex = pages.length - 1;
       var pageIndex = pages.indexOf(currentPage);
       var direction = pageIndex + incrementer;
+      $scope.slidingDirection = (incrementer === 1) ? 'slide-right' : 'slide-left';
       if (direction === -1) { 
         direction = lastPageIndex;
       }
